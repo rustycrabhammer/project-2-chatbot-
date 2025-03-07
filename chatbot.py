34c4ca1a05cb2ba9fae5241d6c7f7570732deb5f
rustycrabhammer <nikki.sai06@gmail.com>
2025-03-07 09:34:56 +0530

    Update chatbot.pyas asfa

--- chatbot.py
+++ chatbot.py
@@ -23,7 +23,7 @@ if "messages" not in st.session_state.keys(): # Initialize the chat message hist
 # llm = ChatOpenAI(model_name="gpt-4o-mini")
 # llm = ChatGoogleGenerativeAI(model = "gemini-pro")
 llm = ChatOpenAI(model = "meta-llama/Llama-3.2-90B-Vision-Instruct-Turbo",
-                      openai_api_key = st.secrets["TOGETHER_API_KEY"] , ## use your key
+                      openai_api_key = st.secrets["77de2293594e8be804cc199d6701d8d45368669afe7430e3b4cd1cc398d1e168"] , ## use your key
                       openai_api_base = "https://api.together.xyz/v1"
 
 )
